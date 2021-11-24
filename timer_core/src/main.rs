77d82ffe3570c43666037ee842d96b72463c6ced
Pando85 <pando855@gmail.com>
2021-11-24 21:59:02 +0100

    feat: Basic implementation of draw and counter

--- timer_core/src/main.rs
+++ timer_core/src/main.rs
@@ -1,81 +1,70 @@
-use crossterm::{
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use std::{
-    error::Error,
-    io,
-    time::{Duration, Instant},
-};
-use tui::{
-    backend::{Backend, CrosstermBackend},
-    layout::Alignment,
-    style::{Color, Modifier, Style},
-    text::{Span, Spans},
-    widgets::Paragraph,
-    Frame, Terminal,
-};
+//! Demonstrates how to read events asynchronously with tokio.
+//!
+//! cargo run --features="event-stream" --example event-stream-tokio
 
-fn main() -> Result<(), Box<dyn Error>> {
-    // setup terminal
-    enable_raw_mode()?;
+use std::io;
+use std::time::{Duration, SystemTime};
+use std::thread::sleep;
+
+use crossterm::cursor;
+use crossterm::execute;
+use crossterm::style;
+use crossterm::terminal;
+use crossterm::Result;
+
+
+fn print_events<W>(w: &mut W) -> Result<()>
+where
+    W: io::Write,
+{
+    execute!(w, terminal::EnterAlternateScreen)?;
+
+    let now = SystemTime::now();
+    let total = Duration::from_secs(10);
+
+    loop {
+        sleep(Duration::from_secs(1));
+
+        let elapsed_time = now.elapsed().unwrap();
+        let counter = total.saturating_sub(elapsed_time);
+        draw(w, counter)?;
+        if counter.is_zero() {
+            sleep(Duration::from_secs(5));
+            return Ok(());
+        }
+    }
+}
+
+
+fn draw<W>(w: &mut W, counter: Duration) -> Result<()>
+where
+    W: io::Write,
+{
+    execute!(
+        w,
+        terminal::Clear(terminal::ClearType::All),
+        cursor::Hide,
+        cursor::MoveTo(0, 0)
+    )?;
+    println!("{:?}", counter);
+    Ok(())
+}
+
+fn main() -> Result<()> {
     let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
 
-    // create app and run it
-    let tick_rate = Duration::from_secs(1);
-    let res = run_app(&mut terminal, tick_rate);
+    let res = print_events(&mut stdout);
 
-    // restore terminal
-    disable_raw_mode()?;
-    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
-    terminal.show_cursor()?;
+    execute!(
+        stdout,
+        style::ResetColor,
+        cursor::Show,
+        terminal::LeaveAlternateScreen
+    )?;
 
     if let Err(err) = res {
         println!("{:?}", err)
     }
 
     Ok(())
-}
-
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, tick_rate: Duration) -> io::Result<()> {
-    let mut last_tick = Instant::now();
-    loop {
-        terminal.draw(|f| ui(f))?;
-
-        if last_tick.elapsed() >= tick_rate {
-            last_tick = Instant::now();
-        }
-    }
-}
-
-fn ui<B: Backend>(f: &mut Frame<B>) {
-    let size = f.size();
-
-    let text = vec![
-        Spans::from("This is a line "),
-        Spans::from(Span::styled(
-            "This is a line   ",
-            Style::default().fg(Color::Red),
-        )),
-        Spans::from(Span::styled(
-            "This is a line",
-            Style::default().bg(Color::Blue),
-        )),
-        Spans::from(Span::styled(
-            "This is a longer line",
-            Style::default().add_modifier(Modifier::CROSSED_OUT),
-        )),
-        Spans::from(Span::styled(
-            "This is a line",
-            Style::default()
-                .fg(Color::Green)
-                .add_modifier(Modifier::ITALIC),
-        )),
-    ];
-
-    let paragraph = Paragraph::new(text.clone()).alignment(Alignment::Center);
-    f.render_widget(paragraph, size);
 }
