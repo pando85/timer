13b3db4bcef5a9d3da03c22cc60880c0e5a5dee1
Pando85 <pando855@gmail.com>
2021-11-27 23:47:16 +0100

    feat: Add input parser

--- timer_core/src/main.rs
+++ timer_core/src/main.rs
@@ -4,9 +4,25 @@ use timer_core::ui;
 use std::io;
 use std::time::{Duration, SystemTime};
 
-fn main() {
-    let mut stdout = io::stdout();
+use clap::{crate_authors, crate_description, crate_version, Parser};
 
+#[derive(Parser)]
+#[clap(
+    name="timer",
+    about = crate_description!(),
+    version = crate_version!(),
+    author = crate_authors!("\n"),
+)]
+struct Opts {
+    /// Remaining time until the alarm sounds
+    time: String,
+}
+
+fn main() {
+    let opts: Opts = Opts::parse();
+
+    let _counter = timer::Time::new(opts.time.as_str());
+    let mut stdout = io::stdout();
     ui::set_up_terminal(&mut stdout).unwrap();
 
     let now = SystemTime::now();
