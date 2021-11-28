70a0b9699ece25d8ddd131afeb2846ddfadf8f4a
Pando85 <pando855@gmail.com>
2021-11-28 20:04:04 +0100

    refactor: Remove Time type and fix regex

--- timer_core/src/timer.rs
+++ timer_core/src/timer.rs
@@ -11,43 +11,42 @@ use regex::{Regex, RegexSet};
 #[derive(Debug, PartialEq)]
 pub struct Time(Duration);
 
-impl Time {
-    pub fn new(s: &str) -> Result<Self> {
-        let re = Regex::new(
-            r"(?x)
-            (?:(?P<hours>\d+)h ?)?
-            (?:(?P<minutes>\d+)m ?)?
-            (?:(?P<seconds>\d+)s ?)?$",
-        )
+pub fn parse_time(s: &str) -> Result<Duration> {
+    let re = Regex::new(r"(?:(?P<hours>\d+)h ?)?(?:(?P<minutes>\d+)m ?)?(?:(?P<seconds>\d+)s ?)?$")
         .unwrap();
-        // without `^(?!$)` match anything
 
-        //let caps = re.captures(s).ok_or(err: E)?;
-        let caps = re.captures(s).unwrap();
-        // if exists
-        let set = RegexSet::new(&[r"(\d+)h", r"(\d+)m", r"(\d+)s"]).unwrap();
+    // let caps = re.captures(s).ok_or(err: E)?;
+    let caps = re.captures(s).unwrap();
 
-        let matches: Vec<_> = set.matches(s).into_iter().collect();
+    // regex without `^(?!$)` must ensure of matching something
+    // add error
+    if caps[0].is_empty() {
+        panic!("No valid time found.")
+    };
 
-        let hours = if matches.contains(&0) {
-            caps["hours"].parse::<u32>().unwrap()
-        } else {
-            0
-        };
-        let minutes = if matches.contains(&1) {
-            caps["minutes"].parse::<u32>().unwrap()
-        } else {
-            0
-        };
-        let seconds = if matches.contains(&2) {
-            caps["seconds"].parse::<u32>().unwrap()
-        } else {
-            0
-        };
+    // if exists
+    let set = RegexSet::new(&[r"(\d+)h", r"(\d+)m", r"(\d+)s"]).unwrap();
 
-        let total: u64 = (hours * 3600 + minutes * 60 + seconds).into();
-        Ok(Time(Duration::from_secs(total)))
-    }
+    let matches: Vec<_> = set.matches(s).into_iter().collect();
+
+    let hours = if matches.contains(&0) {
+        caps["hours"].parse::<u32>().unwrap()
+    } else {
+        0
+    };
+    let minutes = if matches.contains(&1) {
+        caps["minutes"].parse::<u32>().unwrap()
+    } else {
+        0
+    };
+    let seconds = if matches.contains(&2) {
+        caps["seconds"].parse::<u32>().unwrap()
+    } else {
+        0
+    };
+
+    let total: u64 = (hours * 3600 + minutes * 60 + seconds).into();
+    Ok(Duration::from_secs(total))
 }
 
 pub fn countdown<W>(w: &mut W, end: SystemTime) -> Result<()>
@@ -74,12 +73,10 @@ mod tests {
 
     #[test]
     fn test_time_from_string() {
-        assert_eq!(Time(Duration::from_secs(7800)), Time::new("2h10m").unwrap());
-        assert_eq!(Time(Duration::from_secs(70)), Time::new("1m10s").unwrap());
-        assert_eq!(Time(Duration::from_secs(420)), Time::new("5m120s").unwrap());
-        assert_eq!(
-            Time(Duration::from_secs(35996400)),
-            Time::new("9999h").unwrap()
-        );
+        assert_eq!(Duration::from_secs(7800), parse_time("2h10m").unwrap());
+        assert_eq!(Duration::from_secs(70), parse_time("1m10s").unwrap());
+        assert_eq!(Duration::from_secs(420), parse_time("5m120s").unwrap());
+        assert_eq!(Duration::from_secs(603), parse_time("10m3s").unwrap());
+        assert_eq!(Duration::from_secs(35996400), parse_time("9999h").unwrap());
     }
 }
