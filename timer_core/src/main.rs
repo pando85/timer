6f8a460269f5fe3e681690a04e2ab598722e9a25
Pando85 <pando855@gmail.com>
2021-12-04 23:51:36 +0100

    feat: Add signal handling for graceful shutdown

--- timer_core/src/main.rs
+++ timer_core/src/main.rs
@@ -1,7 +1,12 @@
+use std::time::Duration;
+use timer_core::beep::beep;
 use timer_core::timer;
 use timer_core::ui;
 
+use signal_hook::{consts::signal::*, iterator::Signals};
 use std::io;
+use std::process::exit;
+use std::thread;
 use std::time::SystemTime;
 
 use clap::{crate_authors, crate_description, crate_version, Parser};
@@ -18,20 +23,44 @@ struct Opts {
     time: String,
 }
 
-fn main() {
+fn main() -> Result<(), io::Error> {
     let opts: Opts = Opts::parse();
 
     let counter = timer::parse_time(opts.time.as_str()).unwrap();
     let mut stdout = io::stdout();
     ui::set_up_terminal(&mut stdout).unwrap();
 
+    let mut signals = Signals::new(&[SIGWINCH, SIGTERM, SIGINT, SIGQUIT]).unwrap();
+
     let now = SystemTime::now();
     let end = now + counter;
-    let res = timer::countdown(&mut stdout, end);
 
-    ui::restore_terminal(&mut stdout).unwrap();
+    thread::spawn(move || {
+        match timer::countdown(&mut stdout, end) {
+            Ok(_) => {}
+            Err(err) => {
+                println!("{:?}", err);
+            }
+        };
+        ui::restore_terminal(&mut stdout).unwrap();
+    });
 
-    if let Err(err) = res {
-        println!("{:?}", err)
+    for signal in signals.forever() {
+        match signal {
+            SIGWINCH => {
+                // resize terminal
+            }
+            SIGTERM | SIGINT | SIGQUIT => {
+                let mut stdout = io::stdout();
+
+                // ensure beep stops
+                beep(0, Duration::from_secs(0)).unwrap();
+                ui::restore_terminal(&mut stdout).unwrap();
+                exit(1);
+            }
+            _ => unreachable!(),
+        }
     }
+
+    Ok(())
 }
