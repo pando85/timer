2ebde79c2fd3726212b318875c7ac7c49cda2cb0
Pando85 <pando855@gmail.com>
2021-12-04 20:40:38 +0100

    feat: Add center function to printed text

--- timer_core/src/ui.rs
+++ timer_core/src/ui.rs
@@ -18,9 +18,12 @@ where
         cursor::MoveTo(0, 0)
     )?;
     let counter_string = time_to_string(counter);
-    // get terminal size
-    // get centered output
-    println!("{}", counter_string);
+    let size = terminal::size()?;
+    let s = match center(size, counter_string.clone()) {
+        Some(s) => s,
+        None => counter_string,
+    };
+    println!("{}", s);
     Ok(())
 }
 
@@ -66,6 +69,45 @@ fn time_to_string(counter: Duration) -> String {
     }
 }
 
+fn center(size: (u16, u16), s: String) -> Option<String> {
+    let s_size = get_size(&s);
+    let distance = get_distance_from_top_left(size, s_size)?;
+    let horizontal_space = vec![" "; distance.0 as usize].join("");
+    let vertical_space = vec!["\n"; distance.1 as usize].join("");
+    let horizontal_centered_s = s
+        .split('\n')
+        .map(|s| horizontal_space.clone() + s)
+        .collect::<Vec<String>>()
+        .join("\n");
+    Some(vertical_space + &horizontal_centered_s)
+}
+
+/// Return text size (columns, rows)
+fn get_size(s: &str) -> (u16, u16) {
+    let v: Vec<&str> = s.split('\n').collect();
+    let columns = v
+        .clone()
+        .into_iter()
+        .max_by(|a, b| a.chars().count().cmp(&b.chars().count()))
+        .unwrap()
+        .chars()
+        .count();
+    let rows = v.len();
+    (columns as u16, rows as u16)
+}
+
+/// Return distance from top left corner (columns, rows)
+fn get_distance_from_top_left(
+    terminal_size: (u16, u16),
+    text_size: (u16, u16),
+) -> Option<(u16, u16)> {
+    let get_distance = |x: u16, y: u16| -> Option<u16> { Some(x.checked_sub(y)? / 2) };
+    Some((
+        get_distance(terminal_size.0, text_size.0)?,
+        get_distance(terminal_size.1, text_size.1)?,
+    ))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -94,5 +136,27 @@ mod tests {
             "4s".to_string()
         );
         assert_eq!(time_to_string(Duration::from_secs(0)), "0s".to_string());
+    }
+
+    #[test]
+    fn test_get_size() {
+        assert_eq!(get_size("\n\n\n\n\n"), (0, 6));
+        assert_eq!(get_size("123456789"), (9, 1));
+        assert_eq!(get_size("123\n1234\n1234\n"), (4, 4));
+        assert_eq!(get_size("1\n1\n1234567"), (7, 3));
+        assert_eq!(get_size(""), (0, 1));
+    }
+
+    #[test]
+    fn test_get_distance_from_top_left() {
+        assert_eq!(
+            get_distance_from_top_left((10, 10), (5, 5)).unwrap(),
+            (2, 2)
+        );
+        assert_eq!(
+            get_distance_from_top_left((100, 100), (20, 20)).unwrap(),
+            (40, 40)
+        );
+        assert_eq!(get_distance_from_top_left((100, 100), (2000, 2000)), None);
     }
 }
