2873c4239b18acb81b81f957820bef26ea89ee42
Alexander Gil <pando855@gmail.com>
2022-10-04 17:40:34 +0200

    refactor: Replace clap function with command or arg
    Signed-off-by: Alexander Gil <pando855@gmail.com>

--- timer_core/src/opts.rs
+++ timer_core/src/opts.rs
@@ -1,7 +1,7 @@
 use clap::{crate_authors, crate_description, crate_version, ArgAction, Parser};
 
 #[derive(Parser)]
-#[clap(
+#[command(
     name="timer",
     about = crate_description!(),
     version = crate_version!(),
@@ -9,17 +9,17 @@ use clap::{crate_authors, crate_description, crate_version, ArgAction, Parser};
 )]
 pub struct Opts {
     /// Repeat countdown infinitely
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub r#loop: bool,
     /// Disable playing sounds
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub silence: bool,
     /// Enable terminal bell (useful for visual bell)
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub terminal_bell: bool,
     /// Remaining time until the alarm sounds. Format: `%Hh %Mm %Ss`.
     /// It also supports `min` for minutes or empty for seconds.
     /// In addition, you can set a target time `%H:%M`. E.g.: 10s, 12:00, 3h10m, 15min, 10.
-    #[clap(action = ArgAction::Append, number_of_values = 1)]
+    #[arg(action = ArgAction::Append, num_args = 1)]
     pub time: Vec<String>,
 }
