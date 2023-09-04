e2b476779b8a427e2e1ae7806c97b8b8df506111
Alexander Gil <pando855@gmail.com>
2023-09-04 08:53:48 +0200

    build: Upgrade dependencies
    Signed-off-by: Alexander Gil <pando855@gmail.com>

--- timer_core/src/ui.rs
+++ timer_core/src/ui.rs
@@ -1,4 +1,5 @@
 use crate::time::Time;
+use crate::Result;
 
 use std::io;
 
@@ -6,7 +7,6 @@ use crossterm::cursor;
 use crossterm::execute;
 use crossterm::style;
 use crossterm::terminal;
-use crossterm::Result;
 use time::Duration;
 
 pub fn draw<W>(w: &mut W, counter: Duration) -> Result<()>
@@ -27,14 +27,14 @@ where
     Ok(())
 }
 
-pub fn set_up_terminal<W>(w: &mut W) -> Result<()>
+pub fn set_up_terminal<W>(w: &mut W) -> std::io::Result<()>
 where
     W: io::Write,
 {
     execute!(w, terminal::EnterAlternateScreen, cursor::Hide)
 }
 
-pub fn restore_terminal<W>(w: &mut W) -> Result<()>
+pub fn restore_terminal<W>(w: &mut W) -> std::io::Result<()>
 where
     W: io::Write,
 {
