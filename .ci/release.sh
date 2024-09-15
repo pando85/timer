61d9efa9385a6858eb92a6cf6bea97d30317ddca
Alexander Gil <pando855@gmail.com>
2024-09-15 22:01:20 +0200

    refactor(ci): Change comments in `release.sh`
    Signed-off-by: Alexander Gil <pando855@gmail.com>

--- .ci/release.sh
+++ .ci/release.sh
@@ -7,10 +7,10 @@ vim timer_core/Cargo.toml
 # update lock file
 cargo update -p timer_core
 
-# update CHANGELOG.md
 make update-changelog
 
-# merge PR
 git add .
 VERSION=$(sed -n 's/^version = "\(.*\)"/\1/p' timer_core/Cargo.toml | head -n1)
 git commit -m "release: Version $VERSION"
+
+echo "After merging the PR, tag and release are automatically done"
