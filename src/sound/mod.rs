37416e8e1814ede6fb2a5d16ca424fad49f184b5
Alexander Gil <pando855@gmail.com>
2025-07-13 07:31:08 +0200

    build(deps): Update API of rodio 0.21
    Signed-off-by: Alexander Gil <pando855@gmail.com>

--- src/sound/mod.rs
+++ src/sound/mod.rs
@@ -1,32 +1,33 @@
 use crate::Result;
 
-use std::io::{BufReader, Cursor};
-
-use rodio::{Decoder, OutputStream, OutputStreamHandle, source::Source};
+use rodio::{Decoder, OutputStream, OutputStreamBuilder, Sink};
+use std::convert::TryFrom;
+use std::io::Cursor;
 
 pub struct Sound {
     _stream: OutputStream,
-    stream_handle: OutputStreamHandle,
     cursor: Cursor<&'static [u8; 5943]>,
 }
 
 impl Sound {
     pub fn new() -> Result<Self> {
-        let (stream, stream_handle) = OutputStream::try_default()?;
+        let stream = OutputStreamBuilder::open_default_stream()?;
         let contents = include_bytes!("beep.ogg");
         Ok(Sound {
             _stream: stream,
-            stream_handle,
             cursor: Cursor::new(contents),
         })
     }
 
     /// Play sound (it has a delay of ~100ms)
     pub fn play(&self) -> Result<()> {
-        let buffer = BufReader::new(self.cursor.clone());
-        let decoded = Decoder::new_vorbis(buffer).unwrap();
-        let source = decoded.convert_samples();
-        self.stream_handle.play_raw(source)?;
+        let stream = OutputStreamBuilder::open_default_stream()?;
+        let mixer = stream.mixer();
+        let cursor = self.cursor.clone();
+        let decoder = Decoder::try_from(cursor)?;
+        let sink = Sink::connect_new(mixer);
+        sink.append(decoder);
+        sink.sleep_until_end();
         Ok(())
     }
 }
