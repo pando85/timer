318dc8c629847cd3d6828b6d3e3c7d688ce87d88
Alexander Gil <pando855@gmail.com>
2025-07-13 09:10:48 +0200

    fix: Reproduce rodio 0.20 play behaviour
    Signed-off-by: Alexander Gil <pando855@gmail.com>

--- src/sound/mod.rs
+++ src/sound/mod.rs
@@ -1,31 +1,33 @@
 use crate::Result;
 
-use rodio::{Decoder, OutputStream, OutputStreamBuilder, Sink};
-use std::convert::TryFrom;
 use std::io::Cursor;
+
+use rodio::mixer::Mixer;
+use rodio::stream::{OutputStream, OutputStreamBuilder};
 
 pub struct Sound {
     _stream: OutputStream,
+    mixer: Mixer,
     cursor: Cursor<&'static [u8; 5943]>,
 }
 
 impl Sound {
     pub fn new() -> Result<Self> {
         let stream = OutputStreamBuilder::open_default_stream()?;
+        let mixer = stream.mixer().clone();
         let contents = include_bytes!("beep.ogg");
         Ok(Sound {
             _stream: stream,
+            mixer,
             cursor: Cursor::new(contents),
         })
     }
 
     /// Play sound (it has a delay of ~100ms)
     pub fn play(&self) -> Result<()> {
-        let stream = OutputStreamBuilder::open_default_stream()?;
-        let mixer = stream.mixer();
         let cursor = self.cursor.clone();
-        let decoder = Decoder::try_from(cursor)?;
-        let sink = Sink::connect_new(mixer);
+        let decoder = rodio::Decoder::new(cursor)?;
+        let sink = rodio::Sink::connect_new(&self.mixer);
         sink.append(decoder);
         sink.sleep_until_end();
         Ok(())
